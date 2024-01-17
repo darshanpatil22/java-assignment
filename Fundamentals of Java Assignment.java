0223654c785c5af0f6d8fc62d8b789c0b1d1faa1
darshanpatil22 <138565504+darshanpatil22@users.noreply.github.com>
2024-01-17 10:53:42 +0530

    Delete Fundamentals of Java Assignment.java

--- Fundamentals of Java Assignment.java
+++
@@ -1,6 +0,0 @@
-J@ Ehat is ProAramminA lanAuaAe /
-1@ Eh0 do we need a *roAramminA lanAuaAe/
-)@ Ehat are the features of Java/
-D@ Ehat is an 'b3ect/
-2@ Ehat is a class/
-&@ Ex*lain about the main() method in Java?
